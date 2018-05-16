67b7446b9290467d53f5eb44bc45eb891edc6575
zhangxin516 <zhangxin1992pm@gmail.com>
2018-05-16 17:02:27 +0800

    Fix bug: stop command.

--- src/main/bin/stop.sh
+++ src/main/bin/stop.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-BASEDIR=`dirname $0`/../lib
-BASEDIR=`(cd "$BASEDIR"; pwd)`
+BASEDIR=$(cd $(dirname $0); cd ..; pwd -P)
 FEATURE="uni.mlgb.onlyapp.shit.Application"
 
-pidnum=`ps -ef|grep "$BASEDIR $FEATURE"|grep -v grep|wc -l`
+pidnum=`ps -ef|grep "$FEATURE"|grep -v grep|wc -l`
 if [ $pidnum -lt 1 ]
 	then
 		echo "no program killed."
 	else
-	    for pid in `ps -ef|grep "$BASEDIR $FEATURE"|grep -v grep|awk '{print $2}'`
+	    for pid in `ps -ef|grep "$FEATURE"|grep -v grep|awk '{print $2}'`
 		do
-			kill -15 $pid
+			kill -9 $pid
 			echo "$pid stoped."
 		done
 		sleep 1
